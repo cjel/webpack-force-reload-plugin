2917fe8c7d10ece0759a6a93d7d2bbd8aa1209da
Philipp Dieter <philipp.dieter@attic-media.net>
2024-06-24 20:08:43 +0200

    Remove debugging output, require needed libraries

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const { glob }                   = require('glob')
+const _                          = require('lodash');
 const HtmlWebpackPlugin          = require('html-webpack-plugin');
+const path                       = require("path");
+const { glob }                   = require('glob');
 
 class WebpackForceReload {
   constructor(options = {}) {
     let that = this;
     this.options = options;
     this.resolvedFiles = [];
-    console.debug(this.options);
     glob.glob(
       this.options.files,
       {
         cwd: this.options.cwd
       },
       function(err, matches) {
-        console.debug(matches);
         _.forEach(matches, function(value) {
           var resolvedFile = path.resolve(value);
           that.resolvedFiles.push(resolvedFile);
         }
       );
     });
-    console.debug(this.resolvedFiles);
   }
   apply (compiler) {
     compiler.hooks.compilation.tap('WebpackForceReload', (compilation) => {
